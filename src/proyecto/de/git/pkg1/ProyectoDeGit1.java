ad1e3bbf508e1307095d5c362b9c787445a62541
Jose Pajoc <jose.ernesto.pajoc@gmail.com>
2024-07-09 07:26:27 -0600

    creando repositorio

---
+++ src/proyecto/de/git/pkg1/ProyectoDeGit1.java
@@ -0,0 +1,15 @@
+
+package proyecto.de.git.pkg1;
+
+
+public class ProyectoDeGit1 {
+
+   
+    public static void main(String[] args) {
+        System.out.println("Mi proyecto");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+    
+}
